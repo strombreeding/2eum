1b2f5990915fb2f92dd8c05fba32b0023a440951
strombreeding <speaker1403@gmail.com>
2022-10-19 18:38:30 +0900

    test

--- static/js/formsend.js
+++ static/js/formsend.js
@@ -1,17 +1,17 @@
+const zzz = document.querySelector(".zzz")
 const zz = async(e)=>{
   e.preventDefault()
-const form = document.getElementById("zz") 
-const formData = new FormData(form)
-await $.ajax({
-    type:"Post",
-    url:`https://wetube-jinytree.herokuapp.com/api/test`,
-    data:formData,
-    dataType:"JSON",
-    processData:false,
-    contentType:false,
-    success : (response)=>{
-        alert(response.msg)
-    }
-})
+  const form = document.getElementById("zz") 
+  const a = form.value
+  console.log(a)
+  await $.ajax({
+      type:"Post",
+      url:`https://wetube-jinytree.herokuapp.com/api/test`,
+      data:{a},
+      dataType:"JSON",
+      success : (response)=>{
+          alert(response.data.a)
+      }
+  })
 }
 zzz.addEventListener("submit",zz)
