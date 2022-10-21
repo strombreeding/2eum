bc57aed730975fbbfda4fae791fe49a0a9bf3c74
strombreeding <speaker1403@gmail.com>
2022-10-21 11:16:06 +0900

    api login

--- static/js/login-sosial.js
+++ static/js/login-sosial.js
@@ -1,15 +1,21 @@
 const login__google = async()=>{
+    const code = new URL(window.location.href).searchParams.get("code")
+    console.log(code)
     console.log("google")
-    console.log(baseUrl)
     await $.ajax({
-        url: "https://wetube-jinytree.herokuapp.com/2eum/google/start",
-        type: "GET",
-        success: function(response){
-            alert(response);
-        }
-    });
+            url: `https://wetube-jinytree.herokuapp.com/2eum/auth/google/callback?code=${code}`,
+            type: "GET",
+            success: function(res){
+                console.log(res)
+                console.log(res.msg)
+                console.log(res.statusCode)
+                console.log(res.data)
+                alert(`${res.msg}\n${res.statusCode}\n${res.data}`)
+                }
+            });
 }
-const login__kakao = async()=>{
+window.addEventListener("load",login__google)
+// const login__kakao = async()=>{
     
-    console.log("kakao")
-}
+//     console.log("kakao")
+// }
