e9e9aeea20c70a65b130cbbe2960cd11a1cb9ce9
strombreeding <speaker1403@gmail.com>
2022-10-24 17:03:17 +0900

    logout

--- static/js/html/loginBtn.js
+++ static/js/html/loginBtn.js
@@ -1,18 +1,18 @@
-const home__view = ()=>{
-  const home = document.getElementById("home")
-  const login = document.getElementById("login")
-  home.className=""
-  login.innerHTML=``      
-  console.log("home__view")
-}
-const login__view = async()=>{
-  const article=document.getElementsByTagName("article")[0]
-  while(article.hasChildNodes()){
-    article.removeChild(article.firstChild)
+const home__view = () => {
+  const home = document.getElementById("home");
+  const login = document.getElementById("login");
+  home.className = "";
+  login.innerHTML = ``;
+  console.log("home__view");
+};
+const login__view = async () => {
+  const article = document.getElementsByTagName("article")[0];
+  while (article.hasChildNodes()) {
+    article.removeChild(article.firstChild);
   }
-  console.log("login__view")
-  const login = document.getElementById("login")
-  login.innerHTML=`
+  console.log("login__view");
+  const login = document.getElementById("login");
+  login.innerHTML = `
   <div class="login__div">
   <br>
   <br>
@@ -28,45 +28,46 @@ const login__view = async()=>{
     </a>
   </form>
   </div>
-  `
-}
+  `;
+};
 
-
-const google = async ()=>{
-  localStorage.setItem("sosial","google")
-  location.href = "https://wetube-jinytree.herokuapp.com/2eum/google/start"
-}
-const kakao = async ()=>{
-  localStorage.setItem("sosial","kakao")
-  location.href = "https://wetube-jinytree.herokuapp.com/2eum/kakao/start"
-}
+const google = async () => {
+  localStorage.setItem("sosial", "google");
+  location.href = "https://wetube-jinytree.herokuapp.com/2eum/google/start";
+};
+const kakao = async () => {
+  localStorage.setItem("sosial", "kakao");
+  location.href = "https://wetube-jinytree.herokuapp.com/2eum/kakao/start";
+};
 //href="https://wetube-jinytree.herokuapp.com/2eum/google/start"
-const logout = async ()=>{
+const logout = async () => {
   await $.ajax({
-    url:'https://wetube-jinytree.herokuapp.com/2eum/',
-    type:"delete",
-    data:{sessionId:localStorage.getItem("sessionId")},
-    success:(res)=>{
+    url: `https://wetube-jinytree.herokuapp.com/2eum/${localStorage.getItem(
+      "access_token"
+    )}`,
+    type: "delete",
+    success: (res) => {
       alert(res.msg);
-      localStorage.clear()
-      location.reload()
+      localStorage.clear();
+      location.reload();
       // 서버에서 토큰 밸류(session id)를 db.session 에서 조회후 삭제함.
       // 서버에선 미들웨어
     },
-    error:(err)=>{
-      console.log(err)
-      localStorage.clear()
-      location.reload()
-      alert("음..이상함 발견, 그래도 로그아웃완료")
-    }
-  })
+    error: (err) => {
+      alert(err);
+      localStorage.clear();
+      location.reload();
+      alert("음..이상함 발견, 그래도 로그아웃완료");
+    },
+  });
   // document.cookie="access_token=none;expires=Thu, 01 Jan 1999 00:00:10 GMT;"
   // localStorage.removeItem("loggedIn")
   // localStorage.removeItem("avatarUrl")
   // localStorage.removeItem("sosial")
-}
+};
 
-{/* <div class="login__div">
+{
+  /* <div class="login__div">
 <small>소셜 로그인</small>
 <form class="sosial__form">
   <a class="sosial__btn" onclick="login__google()">
@@ -77,4 +78,5 @@ const logout = async ()=>{
     <img class="kakao-img" src="https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F7a7edf55-3336-470c-858d-69e14253cb8b%2FUntitled.png?table=block&amp;id=24064121-a0fd-4c70-8d41-7a83b5cc890c&amp;spaceId=beaa8bbc-f504-4c20-b220-9fc699f70e12&amp;width=2000&amp;userId=14cc2ef3-04b9-41f7-9991-3bf06bfcb033&amp;cache=v2"/>
   </a>
 </form>
-</div> */}
+</div> */
+}
