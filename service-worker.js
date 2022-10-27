25c5fbe5c65c48a910ec3c5794d5b674d8362b21
strombreeding <speaker1403@gmail.com>
2022-10-27 13:01:36 +0900

    로컬에선 다운로드 잘됨

--- service-worker.js
+++ service-worker.js
@@ -1,108 +1,33 @@
-"use strict";
-// CODELAB: Update cache names any time any of the cached files change.
-const CACHE_NAME = "static-cache-v6";
-const DATA_CACHE_NAME = "data-cache-v5";
-
-// CODELAB: Add list of files to cache here.
-const FILES_TO_CACHE = [
-  ".",
-  "./index.html",
-  "./scripts/app.js",
-  "./scripts/install.js",
-  "./styles/inline.css",
-];
-
-self.addEventListener("install", (evt) => {
-  console.log("[ServiceWorker] Install");
-  // CODELAB: Precache static resources here.
-  evt.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("[ServiceWorker] Pre-caching offline page");
-      return cache.addAll(FILES_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", (evt) => {
-  console.log("[ServiceWorker] Activate");
-  // CODELAB: Remove previous cached data from disk.
-  evt.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(
-        keyList.map((key) => {
-          if (key !== CACHE_NAME) {
-            console.log("[ServiceWorker] Removing old cache", key);
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", (evt) => {
-  console.log("[ServiceWorker] Fetch", evt.request.url);
-
-  evt.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(evt.request).then((response) => {
-        return response || fetch(evt.request);
-      });
-    })
-  );
-
-  if (evt.request.mode !== "navigate") {
-    // Not a page navigation, bail.
-    return;
-  }
-
-  evt.respondWith(
-    fetch(evt.request).catch(() => {
-      return caches.open(CACHE_NAME).then((cache) => {
-        return cache.match("offline.html");
-      });
-    })
-  );
-});
-
 if ("serviceWorker" in navigator) {
   console.log("서비스우커이따");
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("./service-worker.js") // serviceWorker 파일 경로
-      .then((reg) => {
-        console.log("Service worker registered.", reg);
-      })
-      .catch((e) => console.log(e));
-  });
+  navigator.serviceWorker
+    .register("./service-worker.js") // serviceWorker 파일 경로
+    .then((reg) => {
+      console.log("서비스워커 등록", reg);
+    })
+    .catch((e) => console.log(e));
 }
-
-// const CACHE_NAME = "zz"; // 캐쉬 이름을 설정합니다.
-
-// const FILES_TO_CACHE = [
-//   "/2eum/offline.html", // 캐쉬할 페이지 or 파일 들을 설정합니다.
-// ];
-
-// self.addEventListener("install", (event) => {
-//   event.waitUntil(
-//     // 캐쉬할 페이지들을 전부 캐쉬합니다.
-//     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
-//   );
-// });
-// self.addEventListener("fetch", (event) => {
-//   if (event.request.mode !== "navigate") {
-//     // page navigation 제외
-//     return;
-//   }
-
-//   event.respondWith(
-//     fetch(event.request).catch(() => {
-//       return caches
-//         .open(CACHE_NAME)
-//         .then((cache) => cache.match("/2eum/offline.html"));
-//     })
-//   );
-// });
+const CACHE_NAME = "offline"; // 캐쉬 이름을 설정합니다.
+const FILES_TO_CACHE = [
+  "./offline.html", // 캐쉬할 페이지 or 파일 들을 설정합니다.
+];
+self.addEventListener("install", (event) => {
+  console.log(event);
+  event.waitUntil(
+    // 캐쉬할 페이지들을 전부 캐쉬합니다.
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+  );
+});
+self.addEventListener("fetch", (event) => {
+  if (event.request.mode !== "navigate") {
+    // page navigation 제외
+    return;
+  }
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches
+        .open(CACHE_NAME)
+        .then((cache) => cache.match("/offline.html"));
+    })
+  );
+});
--- static/js/pwa/pwa-install.js
+++ static/js/pwa/pwa-install.js
@@ -1,90 +1,30 @@
-"use strict";
-
-let deferredInstallPrompt = null;
 const btnSave = document.getElementById("btnSave");
-btnSave.addEventListener("click", installPWA);
-
-// CODELAB: Add event listener for beforeinstallprompt event
-window.addEventListener("beforeinstallprompt", saveBeforeInstallPromptEvent);
-
-/**
- * Event handler for beforeinstallprompt event.
- *   Saves the event & shows install button.
- *
- * @param {Event} evt
- */
-function saveBeforeInstallPromptEvent(evt) {
-  // CODELAB: Add code to save event & show the install button.
-  deferredInstallPrompt = evt;
-  btnSave.removeAttribute("hidden");
-}
-
-/**
- * Event handler for btnSave - Does the PWA installation.
- *
- * @param {Event} evt
- */
-function installPWA(evt) {
-  // CODELAB: Add code show install prompt & hide the install button.
-  deferredInstallPrompt.prompt();
-  // Hide the install button, it can't be called twice.
-  evt.srcElement.setAttribute("hidden", true);
-  // CODELAB: Log user response to prompt.
-  deferredInstallPrompt.userChoice.then((choice) => {
-    if (choice.outcome === "accepted") {
-      console.log("User accepted the A2HS prompt", choice);
-    } else {
-      console.log("User dismissed the A2HS prompt", choice);
-    }
-    deferredInstallPrompt = null;
-  });
-}
-
-// CODELAB: Add event listener for appinstalled event
-window.addEventListener("appinstalled", logAppInstalled);
-/**
- * Event handler for appinstalled event.
- *   Log the installation to analytics or save the event somehow.
- *
- * @param {Event} evt
- */
-function logAppInstalled(evt) {
-  // CODELAB: Add code to log the event
-  console.log("Weather App was installed.", evt);
-}
-
-// const btnSave = document.getElementById("btnSave");
-// var deferredPrompt;
-
-// window.addEventListener("beforeinstallprompt", function (e) {
-//   console.log("beforeinstallprompt Event fired");
-//   e.preventDefault();
-//   // Stash the event so it can be triggered later.
-//   deferredPrompt = e;
-
-//   btnSave.className = "";
-//   return false;
-// });
-// // 특정 버튼 클릭 시 설치 시작
-// btnSave.addEventListener("click", function () {
-//   console.log("install");
-//   if (deferredPrompt !== undefined) {
-//     // The user has had a postive interaction with our app and Chrome
-//     // has tried to prompt previously, so let's show the prompt.
-//     deferredPrompt.prompt();
-
-//     // Follow what the user has done with the prompt.
-//     deferredPrompt.userChoice.then(function (choiceResult) {
-//       console.log(choiceResult.outcome);
-
-//       if (choiceResult.outcome == "dismissed") {
-//         console.log("User cancelled home screen install");
-//       } else {
-//         console.log("User added to home screen");
-//       }
-
-//       // We no longer need the prompt.  Clear it up.
-//       deferredPrompt = null;
-//     });
-//   }
-// });
+var deferredPrompt;
+window.addEventListener("beforeinstallprompt", function (e) {
+  console.log("beforeinstallprompt Event fired");
+  btnSave.className = "";
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  return false;
+});
+// 특정 버튼 클릭 시 설치 시작
+btnSave.addEventListener("click", function () {
+  console.log("install");
+  if (deferredPrompt !== undefined) {
+    // The user has had a postive interaction with our app and Chrome
+    // has tried to prompt previously, so let's show the prompt.
+    deferredPrompt.prompt();
+    // Follow what the user has done with the prompt.
+    deferredPrompt.userChoice.then(function (choiceResult) {
+      console.log(choiceResult.outcome);
+      if (choiceResult.outcome == "dismissed") {
+        console.log("User cancelled home screen install");
+      } else {
+        console.log("User added to home screen");
+      }
+      // We no longer need the prompt.  Clear it up.
+      deferredPrompt = null;
+    });
+  }
+});
